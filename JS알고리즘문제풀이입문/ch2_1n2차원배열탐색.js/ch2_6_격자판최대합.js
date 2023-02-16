dcfdadb71e96f3ac217d3a93327f094311add2e0
horz.d <he1236@ajou.ac.kr>
2023-02-16 21:26:51 +0900

    ch2-6-격자판 풀이 추가 & JS코테광탈방지 폴더명 변경

--- JS알고리즘문제풀이입문/ch2_1n2차원배열탐색.js/ch2_6_격자판최대합.js
+++ JS알고리즘문제풀이입문/ch2_1n2차원배열탐색.js/ch2_6_격자판최대합.js
@@ -4,29 +4,56 @@ const input =
 let [n, ...board] = input.split("\n");
 [...board] = board.map((line) => line.split(" ").map((v) => Number(v)));
 
-// sol 1) 2중 for문 내에서, 인덱스 규칙 기반 모든 합 계산 O(N^2)
+/* 아래의 두개 풀이에서 주요하게 다른점은, 대각선 처리 */
+
+const start = new Date().getTime();
+
+// // sol 1) 2중 for문 내에서, 인덱스 규칙 기반 모든 합 계산 O(N^2)
+// function solution(board) {
+//   let n = board.length;
+//   let ans = [
+//     Array.from({ length: n }, () => 0),
+//     Array.from({ length: n }, () => 0),
+//     [0, 0],
+//   ];
+//   for (let i = 0; i < board.length; i++) {
+//     for (j = 0; j < board.length; j++) {
+//       ans[0][i] += board[i][j];
+
+//       ans[1][j] += board[i][j];
+
+//       if (i === j) {
+//         ans[2][0] += board[i][j];
+//       }
+//       if (i + j === 4) ans[2][1] += board[i][j];
+//     }
+//   }
+//   return Math.max(...ans[0], ...ans[1], ...ans[2]);
+// }
+
+// sol 2 ) O(n^2 + n)
 function solution(board) {
+  let ans = Number.MIN_SAFE_INTEGER;
   let n = board.length;
-  let ans = [
-    Array.from({ length: n }, () => 0),
-    Array.from({ length: n }, () => 0),
-    [0, 0],
-  ];
-  for (let i = 0; i < board.length; i++) {
-    for (j = 0; j < board.length; j++) {
-      ans[0][i] += board[i][j];
-
-      ans[1][j] += board[i][j];
-
-      if (i === j) {
-        ans[2][0] += board[i][j];
-      }
-      if (i + j === 4) ans[2][1] += board[i][j];
+  let sum1, sum2;
+  for (let i = 0; i < n; i++) {
+    sum1 = sum2 = 0;
+    for (let j = 0; j < n; j++) {
+      sum1 += board[i][j];
+      sum2 += board[j][i];
     }
+    ans = Math.max(ans, sum1, sum2);
   }
-  return Math.max(...ans[0], ...ans[1], ...ans[2]);
+  sum1 = sum2 = 0;
+  for (let i = 0; i < n; i++) {
+    sum1 += board[i][i];
+    sum2 += board[i][n - i - 1];
+  }
+  ans = Math.max(ans, sum1, sum2);
+  return ans;
 }
 
-// sol 2 )
+const end = new Date().getTime();
+console.log(start < end);
 
 console.log(solution(board));
