d8b1b46018aef24735162bc8a02c8b4c30e77c8e
horiz.d <65459616+ABizCho@users.noreply.github.com>
2023-09-19 16:11:26 +0900

    [level 1] Title: 최소직사각형, Time: 3.22 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -1,15 +1,7 @@
 import sys
 
 def solution(sizes):
-    max_width = 0
-    max_height = 0
-    res_total_min = sys.maxsize
+    answer = sys.maxsize
     
-    for cur_w, cur_h in sizes :
-        large, small = max(cur_w, cur_h), min(cur_w, cur_h)
-
-        max_width = max(max_width, large)
-        max_height = max(max_height, small)
-    
-    res_total_min = max_width * max_height
-    return res_total_min
+    answer = max(max(x) for x in sizes) * max(min(y) for y in sizes)
+    return answer
