1469190836ac53281eee8ce006b4524e9804ba54
horiz.d <65459616+ABizCho@users.noreply.github.com>
2023-10-14 09:15:35 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,16 +1,12 @@
 def solution(number, k):
-    # 스택을 사용하여 문제 해결
+    answer = ''
     stack = []
-    for num in number:
-        # 스택이 비어있지 않고, k가 0보다 크고, 스택의 마지막 원소가 현재 숫자보다 작은 경우
-        while stack and k > 0 and stack[-1] < num:
-            # 스택의 마지막 원소 제거
+    
+    for num in number :
+        
+        while stack and k > 0 and stack[-1] < num :
             stack.pop()
-            # k를 1 감소
             k -= 1
-        # 스택에 현재 숫자 추가
         stack.append(num)
-    # 만약 k가 0보다 크다면, 스택에서 k개의 원소 제거
-    stack = stack[:-k] if k > 0 else stack
-    # 스택의 원소들을 합쳐서 문자열로 반환
+    stack = stack[:-k] if k > 0 else stack    
     return ''.join(stack)
